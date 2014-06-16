4d6fca0195e95c932d4ca8327d7f0c2c5fb3bb15
Frank Hale <frankhale@gmail.com>
2014-06-16 00:00:12 -0400

    Added menu / Update README

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 #Ghosts of Yarrow
 
-An exploration in creating a text adventure.
+An exploration in creating a text adventure using Atom-Shell.
 
 ##Author
 
-Frank Hale &lt;frankhale@gmail.com&gt;
+Frank Hale &lt;frankhale@gmail.com&gt;  
+Date: 15 June 2014
 
 ##License
 
--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,29 @@
 var GAME = (function (my) {
-	//window.$ = window.jQuery = require('scripts/jquery-2.1.1.min.js');
+	window.$ = window.jQuery = require('scripts/jquery-2.1.1.min.js');
 
 	var remote = require('remote');
+	var Menu = remote.require('menu');
+	var MenuItem = remote.require('menu-item');
 	var browserWindow = remote.getCurrentWindow();
+	
+	var menuTemplate = [
+		{
+			label: "Developer",
+			submenu: [
+				{
+					label: 'Toggle Dev Tools',
+					click: function() { my.toggleDevTools(); }
+				},
+				{
+					label: 'Reload',
+					click: function() { my.reload(); }				
+				}
+			]
+		}
+	];
+	
+	var devMenu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(devMenu);	
 	
 	var my = {};
 
